1a48bc90ebe4c2d485fe1e7d7d11f3a1a75684a1
CHM <peterconnected@gmail.com>
2022-04-04 15:41:22 +0800

    Added under construction text.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The NTHUGDC homepage is under construction, please come back later!
         </p>
         <a
           className="App-link"
