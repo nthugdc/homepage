ebc32485cb2d9102fb3b6356d4fd426dd79666da
108062328 <sp21804173@gmail.com>
2022-08-19 16:08:46 +0800

    fix scroll

--- app/src/components/home/HomePage.tsx
+++ app/src/components/home/HomePage.tsx
@@ -21,21 +21,55 @@ export default function HomePage() {
     let textY = 400;
 
     useEffect(() => {
-        // gsap animation https://codepen.io/creativeocean/pen/qBbBLyB
-        ScrollTrigger.config({autoRefreshEvents: "DOMContentLoaded,load,visibilitychange"});
+        let mm = gsap.matchMedia();
         gsap.set('.mainscroll', {position:'fixed', background:'#fff', width:'100%', height:'100%', top:0, left:'50%', x:'-50%'})
-        gsap.set('.scrollDist', {width:'100%', height:'200%'})
-        gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:5}})
-            // .fromTo('.aqua', {x:100},{x:140}, 0)
-            .fromTo('.aqua', {y:120},{y:-250}, 0)
-            .fromTo('.cloudmask', {y:345},{y:120}, 0)
-            .fromTo('.mask', {y:100},{y:-800}, 0)
-            // .fromTo('.irys', {x:-100},{x:0}, 0)
-            .fromTo('.irys', {y:300},{y:-220}, 0)
+            gsap.set('.scrollDist', {width:'100%', height:'100%'})
+            gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:5}})
+                // .fromTo('.aqua', {x:100},{x:140}, 0)
+                .fromTo('.aqua', {y:120},{y:-250}, 0)
+                .fromTo('.cloudmask', {y:345},{y:120}, 0)
+                .fromTo('.mask', {y:100},{y:-800}, 0)
+                // .fromTo('.irys', {x:-100},{x:0}, 0)
+                .fromTo('.irys', {y:300},{y:-220}, 0)
+
+        // mm.add("(min-width: 768px)", () => {
+
+        //     gsap.set('.mainscroll', {position:'fixed', background:'#fff', width:'100%', height:'100%', top:0, left:'50%', x:'-50%'})
+        //     gsap.set('.scrollDist', {width:'100%', height:'100%'})
+        //     gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:5}})
+        //         // .fromTo('.aqua', {x:100},{x:140}, 0)
+        //         .fromTo('.aqua', {y:120},{y:-250}, 0)
+        //         .fromTo('.cloudmask', {y:345},{y:120}, 0)
+        //         .fromTo('.mask', {y:100},{y:-800}, 0)
+        //         // .fromTo('.irys', {x:-100},{x:0}, 0)
+        //         .fromTo('.irys', {y:300},{y:-220}, 0)
+
+        //         return () => { // make sure to clean up event listeners in the cleanup function!
+        //             gsap.matchMediaRefresh();
+        //           }
+        // });
+
+        // mm.add("(max-width: 767px", () => {
+        //     // mobile setup code here...
+        //     gsap.globalTimeline.pause();
+        //     // kill gsap
+        //     // gsap.set('.mainscroll', {position:'fixed', background:'#fff', width:'100%', height:'100%', top:0, left:'50%', x:'-50%'})
+        //     // gsap.set('.scrollDist', {width:'100%', height:'200%'})
+        //     // gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:5}})
+        //         // .fromTo('.aqua', {x:100},{x:140}, 0)
+        //         return () => { // make sure to clean up event listeners in the cleanup function!
+        //             gsap.matchMediaRefresh();
+        //           }
+        // });
+
+      
+       
+        // gsap animation https://codepen.io/creativeocean/pen/qBbBLyB
+        
     }, []);
 
     return (
-     <div id="viewport">
+     <div id="viewport" className="h-full md:h-[1200px]">
                 <div className="scrollDist"></div>
                 <div className="mainscroll">
 
@@ -50,7 +84,7 @@ export default function HomePage() {
 
                         <g className="mask">
                             <rect fill="#fff" width="100%" height="801" y="799" />
-                            <image className="cloudmask" href="images/20mask.jpg" xlinkHref="images/20mask.jpg" width="1200" height="800" />
+                            <image className="cloudmask hidden md:block" href="images/20mask.jpg" xlinkHref="images/20mask.jpg" width="1200" height="800" />
                         </g>
                         </mask>
                         <rect fill="#000" width="100%" height="801" y="799" />
@@ -62,7 +96,7 @@ export default function HomePage() {
                         <g mask="url(#m)">
                         
                             <rect fill="#fff" width="100%" height="100%" />     
-                                <image className="irys" href="images/16.jpg" xlinkHref="images/16.jpg" width="1300"  /> 
+                                <image className="irys hidden md:block" href="images/16.jpg" xlinkHref="images/16.jpg" width="1300"  /> 
                                 <a href="https://discord.gg/pw5kc5jWFa" target="_blank" rel="noreferrer">
                                     <text className="HomeTitle text-7xl md:text-8xl lg:text-[99px] text-center"  x="50%" y={textY} fill="#fff" href="https://discord.gg/pw5kc5jWFa">JOIN US ♥</text>
                                 </a>
