58c89bddfc4cb4738c8c65c97e7fcf05f9d1dd95
108062328 <sp21804173@gmail.com>
2022-08-20 16:15:06 +0800

    try transform-style: preserve-3d

--- app/src/components/home/HomePage.tsx
+++ app/src/components/home/HomePage.tsx
@@ -22,15 +22,15 @@ export default function HomePage() {
 
     useEffect(() => {
         let mm = gsap.matchMedia();
-        gsap.set('.mainscroll', {position:'fixed', background:'#fff', width:'100%', height:'100%', top:0, left:'50%', x:'-50%'})
-            gsap.set('.scrollDist', {width:'100%', height:'100%'})
-            gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:5}})
-                 .fromTo('.aqua', {x:50},{x:140}, 0)
-                .fromTo('.aqua', {y:120},{y:-250}, 0)
-                .fromTo('.cloudmask', {y:345},{y:120}, 0)
-                .fromTo('.mask', {y:100},{y:-800}, 0)
-                // .fromTo('.irys', {x:-100},{x:0}, 0)
-                .fromTo('.irys', {y:300},{y:-220}, 0)
+        gsap.set('.mainscroll', {position:'fixed', background:'#000', width:'100%', height:'100%', top:0, left:'50%', x:'-50%'})
+        // gsap.set('.scrollDist', {width:'100%', height:'100%'})
+        gsap.timeline({scrollTrigger:{trigger:'.vue', start:'top top', end:'bottom bottom', scrub:5}})
+                .fromTo('.aqua', {x:120},{x:200}, 0)
+            .fromTo('.aqua', {y:120},{y:-250}, 0)
+            .fromTo('.cloudmask', {y:345},{y:120}, 0)
+            .fromTo('.mask', {y:100},{y:-800}, 0)
+            .fromTo('.irys', {x:-100},{x:0}, 0)
+            .fromTo('.irys', {y:300},{y:-220}, 0)
 
         mm.add("(min-width: 768px)", () => {
 
@@ -60,33 +60,31 @@ export default function HomePage() {
     }, []);
 
     return (
-     <div id="viewport" className="h-full md:h-[1200px]">
+     <div id="viewport" className="vue">
                 <div className="scrollDist"></div>
                 <div className="mainscroll">
-
                 {/* 
                     viewBox : 1200 x 800 -> 400 for 1 page
                     preserveAspectRatio for RWD 
 
                 */}
-                <svg height="100%" width="100%" viewBox="0 0 1200 800" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" className="bg-black" >
+                <svg height="100%" width="100%" viewBox="0 0 1200 800" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" className="bg-black h-full md:h-[1090px]" >
                     <mask id="m">
 
 
                         <g className="mask">
                             <rect fill="#fff" width="100%" height="801" y="799" />
-                            <image className="cloudmask hidden md:block" href="images/20mask.jpg" xlinkHref="images/20mask.jpg" width="1200" height="800" />
+                            <image className="cloudmask hidden md:block" href="images/mask.jpg" width="1200" height="800" />
                         </g>
                         </mask>
-                        <rect fill="#000" width="100%" height="801" y="799" />
 
-                        <image className="aqua" href="images/aqua_smaller.png" xlinkHref="images/aqua_smaller.png"  width="1450" />
+                        <image className="aqua" href="images/aqua_smaller.png" xlinkHref="images/aqua_smaller.png"  width="1300" />
                         <text className="HomeTitle text-7xl md:text-8xl lg:text-[99px] text-center" fill="#fff" x="50%" y={textY}>NTHUGDC</text>
                         <text className="HomeTitle text-2xl md:text-3xl lg:text-4xl text-center" fill="#fff" x="50%" y={textY + 60}>清大遊創社</text>
-                 
-                        <g mask="url(#m)">
+
+
                         
-                            <rect fill="#fff" width="100%" height="100%" />     
+                        <g mask="url(#m)">
                                 <image className="irys " href="images/16.jpg" xlinkHref="images/16.jpg" width="1300"  /> 
                                 <a href="https://discord.gg/pw5kc5jWFa" target="_blank" rel="noreferrer">
                                     <text className="HomeTitle text-7xl md:text-8xl lg:text-[99px] text-center"  x="50%" y={textY} fill="#fff" href="https://discord.gg/pw5kc5jWFa">JOIN US ♥</text>
@@ -94,7 +92,7 @@ export default function HomePage() {
                                 
                             
                         </g>
-                    
+                        
                 </svg>
             </div>
             </div>
