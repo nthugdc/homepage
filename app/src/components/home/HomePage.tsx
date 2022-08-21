3ab1555a12b9b019a81faf1dbc34b0a86a0a78e7
108062328 <sp21804173@gmail.com>
2022-08-21 17:14:16 +0800

    fix missing font

--- app/src/components/home/HomePage.tsx
+++ app/src/components/home/HomePage.tsx
@@ -32,7 +32,7 @@ export default function HomePage() {
         gsap.set('.mainscroll', {position:'fixed', background:'#fff', width:'100%', height:'100%', top:0, left:'50%', x:'-50%'})
         gsap.set('.scrollDist', {width:'100%', height:'100%'})
         gsap.timeline({stagger: 0.5,scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:5}})
-        .fromTo('.aqua', {x:120},{x:200}, 0)
+        .fromTo('.aqua', {x:110},{x:200}, 0)
         .fromTo('.aqua', {y:120},{y:-250}, 0)
         .fromTo('.mask', {y:400},{y:-800}, 0)
         .fromTo('.irys', {x:-100},{x:0}, 0)
@@ -99,7 +99,7 @@ export default function HomePage() {
                                
                                 <image className="irys" href="images/16.jpg" width="1300"/> 
                                 <a href="https://discord.gg/pw5kc5jWFa" target="_blank" rel="noreferrer">
-                                    <text className="HomeTitle text-7xl md:text-8xl lg:text-[99px] text-center"  x="600" y="400" fill="#fff" href="https://discord.gg/pw5kc5jWFa">JOIN US ♥</text>
+                                    <text className="HomeTitle text-7xl md:text-8xl lg:text-[99px] text-center"  x="600" y="400" fill="#fff" href="https://discord.gg/pw5kc5jWFa">JOIN US ❤️</text>
                                 </a>
 
                         </g>
